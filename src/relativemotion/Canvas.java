bd82400f810854b05a2b1a356bf2780f9d8c31a0
matthewludwig1 <36999944+matthewludwig1@users.noreply.github.com>
2019-06-20 10:46:23 -0400

    Update Canvas.java

--- src/relativemotion/Canvas.java
+++ src/relativemotion/Canvas.java
@@ -1,15 +1,19 @@
 /*
-    Matt, Aisik, Michelle, Liam
-    2019-06-13
-    This is the canvas class, which contains code to generate graphics
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
 package relativemotion;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.List;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JPanel;
 
 /**
@@ -18,17 +22,26 @@ import javax.swing.JPanel;
  */
 
 public class Canvas extends JPanel{
-    
-    
+    Physics[] objects = new Physics[3];
+    Canvas(){
+        try {
+            objects[0] = new Rocket(0,0,0,2,2,3,500);
+            objects[1] = new Physics(3,3,3,12,12,12,30);
+            objects[2] = new Physics(25,25,12,23,43,20,3);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Canvas.class.getName()).log(Level.SEVERE, null, ex);
+        }
+}
+    public void doStep(){
+        for (int i=0; i<objects.length;i++){
+        objects[i].go();
+
+        }    
+    }
     public void paint(Graphics g) {
     g.setColor(Color.green);
+    Graphics2D g2d = (Graphics2D) g;
     
-    
-//    for(int i=0; i<RelativeMotion.objects.length; i++){
-//       int x = (int)(RelativeMotion.objects[i].getX());
-//       int y = (int)(RelativeMotion.objects[i].getY());
-//        g.drawRect(x, y, 5, 5);
-//    }
     
 //    for(int z = 0; z < 10; z++){
 //        g.drawRect(RelativeMotion.x+(z*10), RelativeMotion.y+(z*10), RelativeMotion.x/25, RelativeMotion.y/25);
@@ -48,15 +61,20 @@ public class Canvas extends JPanel{
 
     repaint();
   }
-     
+    
+    
+        
     public void trace(Physics object){
             double x = object.getX();
             double y = object.getY();
-            double z = object.getZ();   
+            double z = object.getZ();
+
+            
     }
     
     public void draw(){
     repaint();
-    
     }
+
+    
 }
