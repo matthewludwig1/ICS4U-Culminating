8d4c1e2250f6f06607cd5bfb9e37e78dfc0de65c
matthewludwig1 <36999944+matthewludwig1@users.noreply.github.com>
2019-06-20 10:46:52 -0400

    Update Physics.java

--- src/relativemotion/Physics.java
+++ src/relativemotion/Physics.java
@@ -1,7 +1,7 @@
 /*
     Matt, Aisik, Michelle, Liam
-    2019-06-13
-    This is the template for the physics object(engine)
+    2019-06-11
+    This is the template for the physics object (engine)
  */
 package relativemotion;
 
@@ -10,28 +10,12 @@ package relativemotion;
  * @author lidea9928, malud0519
  */
 public class Physics {
-    // variables declaration
     double [] spacialPositions = new double [3]; 
+    
     Vector [] forces = new Vector[9];
     Vector velocity = new Vector();
     private double mass; 
-    /**
-     * 
-     * @param x
-     * x position
-     * @param y
-     * y position
-     * @param z
-     * z position
-     * @param spdx
-     * x component of speed
-     * @param spdy
-     * y component of speed
-     * @param spdz
-     * z component of speed
-     * @param ms 
-     * object's mass
-     */
+    
     Physics(double x, double y, double z, double spdx, double spdy, double spdz, double ms) { // default
 
         spacialPositions[0] = x;   // x dimension
@@ -68,7 +52,6 @@ public class Physics {
     public double getX() {
         return spacialPositions[0];
     }
-    
     /**
      * 
      * @return spacialPositions[1]
@@ -77,7 +60,6 @@ public class Physics {
     public double getY() {
         return spacialPositions[1];
     }
-    
     /**
      * 
      * @return spacialPositions[2]
@@ -87,25 +69,16 @@ public class Physics {
         return spacialPositions[2];
     }
     
-    /**
-     * Adds components of velocity to the corresponding dimension
-     */ 
+    
+    
     public void go(){
-        spacialPositions[0] += velocity.x();  
+        spacialPositions[0] += velocity.x();  // adds the velocity to the position
         spacialPositions[1] += velocity.y();
         spacialPositions[2] += velocity.z();
-        useTheForce();   
+        useTheForce();  
+       
     }
-    
-    /**
-     * General purpose method for adding force
-     * @param vec 
-     * Forces are added to Vector object, vec
-     * forces[0]: Adds force in x-direction
-     * forces[1]: Adds force in y-direction
-     * forces[2]: Adds force in z-direction
-     */
-    public void addForce(Vector vec){  
+    public void addForce(Vector vec){  // adding any/all forces
         for (int i =1; i<forces.length; i++){
             if(forces[i]== null){
                 forces[i] = vec;
@@ -113,18 +86,12 @@ public class Physics {
         }
     }
     
-    /**
-     * Each measure of velocity will be changed based on the acceleration:
-     * forces[i]: retrieves components of force
-     * velocity: new Vector object with the positions adjusted via forces array
-     * Iterates until end of array
-     */
-    public void useTheForce(){  
+    public void useTheForce(){  // each measure of velocity will be changed based on the acceleration
         for (int i = 0; i < forces.length; i++) { // no enhanced loop, java
             if(forces[i] != null) {
-                velocity = new Vector(velocity.x()+ forces[i].x() / mass, 
-                        velocity.y() + forces[i].y()/ mass,
-                        velocity.z() + forces[i].z()/ mass);  // beautiful equation
+                velocity = new Vector(velocity.x()+ forces[i].x() , 
+                        velocity.y() + forces[i].y(),
+                        velocity.z() + forces[i].z());  // beautiful equation
             }
         }
     }
